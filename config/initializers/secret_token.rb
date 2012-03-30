237427bc5947af989be739c34dead88aee6d30a2
Sasan <siesonz@gmail.com>
2012-03-30 12:09:57 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reversify::Application.config.secret_token = '3866c1293df9e5cf8ebbf59ff41f93667de091fc89818f23f0935262189b18d594890fbd7c7d88088a628cf6b2262ae7c286827976b03561da93f1ec848951d2'
